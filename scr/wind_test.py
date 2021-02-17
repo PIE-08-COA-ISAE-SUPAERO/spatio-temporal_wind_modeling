921550c303335c8bec834c922b6627a004109053
LoicRoux <loic-roux@hotmail.com>
2021-02-17 19:08:19 +0100

    Update wind_test.py

--- scr/wind_test.py
+++ scr/wind_test.py
@@ -5,4 +5,5 @@ d.import_wind_cube("G:/Mon Drive/PIE COA 08/Architecture code/PGM/spatio-tempora
 
 #d.profil_turbulence(47,-113,2000,1)
 #d.plot_wind_cube([0,1000],[0,1000], [0,1000],True)
-#d.plot_wind_surface("x",[47,113],2000,True)
+#d.plot_wind_surface("x",[47,-113],2000,True)
+d.plot_wind_surface("z",[47,-113], 2000, True)

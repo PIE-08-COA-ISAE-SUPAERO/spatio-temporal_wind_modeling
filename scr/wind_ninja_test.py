7678f0a16d62983b3b3ca76b8f4d20fada136046
LoicRoux <loic-roux@hotmail.com>
2021-03-17 09:22:43 +0100

    Delete wind_ninja_test.py

--- scr/wind_ninja_test.py
+++
@@ -1,6 +0,0 @@
-import wind_ninja_functions
-
-input_path =  "C:/Users/loic-/OneDrive/Bureau/test_domainAverage/"
-output_path = input_path
-
-wind_ninja_functions.main(input_path, "domainAverage", output_path)
